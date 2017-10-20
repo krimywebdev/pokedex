126132ddfb4b13e170de2f7b71fd3713f580af87
Server <developer@cirtru.com>
2017-10-20 13:05:44 +0530

    Fixing eslint errors in reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,7 +7,9 @@ import pokemons from './pokemons';
  * Reducers
  */
 
+/* eslint-disable import/prefer-default-export */
 export const reducers = combineReducers({
   routing: routerReducer,
-  pokemons: pokemons,
-})
+  pokemons,
+});
+/* eslint-enable import/prefer-default-export */
