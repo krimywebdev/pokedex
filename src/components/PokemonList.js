fcce3a929f54a4dc9fd4931f2b36e74c7a89327a
Server <developer@cirtru.com>
2017-10-20 14:33:57 +0530

    Rectify linting errors

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,77 +2,69 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Table, ProgressBar, Button, Modal } from 'react-bootstrap';
 import { push } from 'react-router-redux';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 import PokemonListElement from './PokemonListElement';
 import PokemonSeeDetail from './PokemonSeeDetail';
 
-
-
-
 /**
  * Pokemon List Component
  */
 
-export class PokemonList extends React.Component
-{
-
+export class PokemonList extends React.Component {
   /**
    * constructor
    */
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
 
-    //when we don't have any pokemons, update the state with the pokemons
+    // when we don't have any pokemons, update the state with the pokemons
     // list taken from the API
 
-    if(0 === this.props.pokemons.length) {
+    if (this.props.pokemons.length === 0) {
       this.props.dispatch({
-        type: 'pokemonsFetchList'
+        type: 'pokemonsFetchList',
       });
     }
 
-
-    //bind this to all event methods
+    // bind this to all event methods
     this.modalAttributesShow = this.modalAttributesShow.bind(this);
-
   }
 
   modalAttributesShow(event) {
-
     const id = Number(event.target.dataset.id);
 
-    //dispatch action
+    // dispatch action
     this.props.dispatch({
       type: 'pokemons.modalAttributesShow',
-      id: id,
-      pname: event.target.dataset.pname
+      id,
+      pname: event.target.dataset.pname,
     });
 
     this.props.dispatch({
       type: 'pokemonsFetchTypeOfPokemon',
       purl: event.target.dataset.purl,
-      pokemon: this.props.pokemons[id - 1]
+      pokemon: this.props.pokemons[id - 1],
     });
-
   }
 
   render() {
-    var pokemons = this.props.pokemons;
-    var that = this;
+    /* eslint-disable prefer-destructuring */
+    const pokemons = this.props.pokemons;
+    /* eslint-enable prefer-destructuring */
+    const that = this;
 
     const options = {
       paginationSize: 5,
-      paginationShowsTotal: false,  // Enable showing total text
-      clearSearch: true
+      paginationShowsTotal: false, // Enable showing total text
+      clearSearch: true,
     };
 
     function formatAvatar(cell, row) {
       return (
         <img src={cell} className="img-responsive"/>
-        );
+      );
     }
 
     function formatTypes(cell, row) {
@@ -80,61 +72,55 @@ export class PokemonList extends React.Component
         <div><bold>{cell[0]}</bold>
           <br/>
           <bold>{cell[1]}</bold></div>
-        );
+      );
     }
 
     function formatAttributes(cell, row) {
-
       return (
         <Button bsSize='small' data-id={row.id} data-pname={row.pname} data-purl={row.purl}
         onClick={that.modalAttributesShow}>Details</Button>
-        );
+      );
     }
 
-    if(this.props.pokemons.length) {
-
-      return(
+    if (this.props.pokemons.length) {
+      return (
         <div>
           <h2>Pokedex</h2>
           <BootstrapTable data={ pokemons } striped hover condensed options={ options } pagination search>
             <TableHeaderColumn dataField='id' isKey dataSort>ID</TableHeaderColumn>
             <TableHeaderColumn dataField='avatar' dataFormat={formatAvatar}>Avatar</TableHeaderColumn>
             <TableHeaderColumn dataField='pname' dataSort>Name</TableHeaderColumn>
-            <TableHeaderColumn dataField='ptypes' dataFormat={formatTypes} filter={ { type: 'TextFilter' } }>Types</TableHeaderColumn>
+            <TableHeaderColumn dataField='ptypes' dataFormat={formatTypes} filter={ { type: 'TextFilter' } }>
+              Types</TableHeaderColumn>
             <TableHeaderColumn dataField='' dataFormat={formatAttributes}>Attributes</TableHeaderColumn>
           </BootstrapTable>
           <PokemonSeeDetail/>
         </div>
       );
-    } else {
-      //show the loading state
-      return(
-        <div>
-          <h2>Welcome to my Pokedex!</h2>
-          <ProgressBar active now={100}/>
-        </div>
-      );
-
     }
 
+    // else show the loading state
+    return (
+      <div>
+        <h2>Welcome to my Pokedex!</h2>
+        <ProgressBar active now={100}/>
+      </div>
+    );
   }
-
 }
 
-
 PokemonList.PropTypes = {
-  pokemons: React.PropTypes.array
+  pokemons: React.PropTypes.array,
 };
 
-
-//export the connected class
+// export the connected class
 function mapStateToProps(state) {
   return ({
     pokemons: state.pokemons.list || [],
-    page: Number(state.routing.locationBeforeTransitions.query.page) || 1
+    page: Number(state.routing.locationBeforeTransitions.query.page) || 1,
   });
 }
 
-//need to fetch state
-export default connect(mapStateToProps) (PokemonList);
+// need to fetch state
+export default connect(mapStateToProps)(PokemonList);
 
--- src/components/PokemonSeeDetail.js
+++ src/components/PokemonSeeDetail.js
@@ -2,25 +2,22 @@ import React from 'react';
 import { Modal, Button, ProgressBar } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
-class PokemonSeeDetail extends React.Component
-{
+class PokemonSeeDetail extends React.Component {
   /**
    * constructor
    */
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
 
-    //bind this to the event methods
+    // bind this to the event methods
     this.modalHide = this.modalHide.bind(this);
   }
 
-  render()
-  {
-    if(this.props.modal_see_attributes.pokemon
+  render() {
+    if (this.props.modal_see_attributes.pokemon
       && this.props.modal_see_attributes.pokemon.attributes
       && this.props.modal_see_attributes.pokemon.attributes.height) {
-        return (
+      return (
         <Modal show={this.props.modal_see_attributes.show}>
           <Modal.Header>
             <Modal.Title>
@@ -47,57 +44,55 @@ class PokemonSeeDetail extends React.Component
             <Button onClick={this.modalHide} bsStyle='primary'>Close</Button>
           </Modal.Footer>
         </Modal>
-      )
-    } else {
-      return (
-        <Modal show={this.props.modal_see_attributes.show}>
-          <Modal.Header>
-            <Modal.Title>
-            Details about Pokemon&nbsp;
-              <strong>{this.props.modal_see_attributes.pname}</strong>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ProgressBar active now={100}/>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.modalHide} bsStyle='primary'>Close</Button>
-          </Modal.Footer>
-        </Modal>
-
-        );
+      );
     }
+
+    return (
+      <Modal show={this.props.modal_see_attributes.show}>
+        <Modal.Header>
+          <Modal.Title>
+          Details about Pokemon&nbsp;
+            <strong>{this.props.modal_see_attributes.pname}</strong>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ProgressBar active now={100}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.modalHide} bsStyle='primary'>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
   }
   /**
    * close the details modal
    */
-  modalHide(event)
-  {
-    //dispatch an action
+  modalHide(event) {
+    // dispatch an action
     this.props.dispatch({
-      type: 'pokemons.modalHide'
+      type: 'pokemons.modalHide',
     });
   }
 }
 
-//export the connected class
+// export the connected class
 function mapStateToProps(state) {
-  //Set the data fro the pokemon see detail modal
+  // Set the data fro the pokemon see detail modal
   let modal_see_attributes;
-  if(state.pokemons.modal && state.pokemons.modal.see_attributes) {
+  if (state.pokemons.modal && state.pokemons.modal.see_attributes) {
     modal_see_attributes = state.pokemons.modal.see_attributes;
   } else {
     modal_see_attributes = {
       show: false,
       id: 0,
       pname: '',
-      pokemon: {}
-    }
+      pokemon: {},
+    };
   }
 
-  //return
+  // return
   return {
-    modal_see_attributes: modal_see_attributes
-  }
+    modal_see_attributes,
+  };
 }
-export default connect(mapStateToProps) (PokemonSeeDetail);
+export default connect(mapStateToProps)(PokemonSeeDetail);
