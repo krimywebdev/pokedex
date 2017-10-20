d8640ebe8f5d9e53ac0d798138bad63e5e900709
Server <developer@cirtru.com>
2017-10-20 17:37:37 +0530

    src/sagas/pokemons.js

--- src/sagas/pokemons.js
+++ src/sagas/pokemons.js
@@ -5,43 +5,42 @@ import ApiPokemons from '../api/pokemons';
 /**
  * Fetch pokemon's list
  */
-export function* pokemonsFetchList(actionObj) { //consists of action type and page
+export function* pokemonsFetchList(actionObj) { // consists of action type and page
   // call the api to get the pokemons
   const pokemons = yield call(ApiPokemons.getPokemonsByState, actionObj);
 
-  //dispatch the success action with the pokemons attached
+  // dispatch the success action with the pokemons attached
   yield put({
     type: 'pokemons.fetchListSuccess',
-    pokemons: pokemons
+    pokemons,
   });
 }
 
-export function* pokemonsFetchPage(actionObj) { //consists of type and page
+export function* pokemonsFetchPage(actionObj) { // consists of type and page
   // call the api to get the pokemons
   const pokemons = yield call(ApiPokemons.getList, actionObj);
 
-  //dispatch the success action with the pokemons attached
+  // dispatch the success action with the pokemons attached
   yield put({
     type: 'pokemons.fetchPageSuccess',
-    pokemons: pokemons
+    pokemons,
   });
 }
 
 export function* pokemonsResetStateOnPageChange(actionObj) {
-
   yield put({
     type: 'pokemons.resetStateOnPageChangeSuccess',
-    pokemons: []
+    pokemons: [],
   });
 }
 
-export function* pokemonsFetchTypeOfPokemon(actionObj) { //consists of type and page
+export function* pokemonsFetchTypeOfPokemon(actionObj) { // consists of type and page
   // call the api to get the pokemons
   const pokemon = yield call(ApiPokemons.getPokemonById, actionObj);
 
-  //dispatch the success action with the pokemons attached
+  // dispatch the success action with the pokemons attached
   yield put({
     type: 'pokemons.fetchTypeOfPokemonSuccess',
-    pokemon: pokemon
+    pokemon,
   });
 }
