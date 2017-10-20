12e1898bc3167b9e9f89e405a32778bea277d399
Server <developer@cirtru.com>
2017-10-20 11:35:51 +0530

    Fixing eslint errors in src/pages/NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,17 +3,17 @@ import React from 'react';
 /**
  * Not Found Page Component
  */
-export default class NotFound extends React.Component
-{
+export default class NotFound extends React.Component {
   /**
    * Render
    */
-  render()
-  {
-    return(
+  /* eslint-disable class-methods-use-this */
+  render() {
+    return (
       <div>
         <h4>Page Not Found</h4>
       </div>
-      );
+    );
   }
+  /* eslint-enable class-methods-use-this */
 }
